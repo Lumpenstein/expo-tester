9fd007aa5f30a658a857c356082279fb1d12ee84
Jhang Chaussy <jhangchaussy@gmail.com>
2019-03-27 14:42:21 +0100

    Implemented check for fingerprintsensor and facial unlock

--- Root.js
+++ Root.js
@@ -3,12 +3,61 @@ import { StyleSheet, Text, View } from 'react-native';
 import {Provider} from 'react-redux'
 import configureStore from './lib/redux/configureStore'
 import {PersistGate} from 'redux-persist/integration/react'
+import { LocalAuthentication } from 'expo'
 
 import MainScreen from './screens/MainScreen'
 
 const persistorStore = configureStore();
 
 class Root extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasHardwareAuth: undefined,
+      hasFingerPrint: undefined,
+      hasFaceUnlock: undefined
+    }
+  }
+
+  _checkHardwareAuth() {
+    LocalAuthentication.hasHardwareAsync()
+        .then(res => {
+          console.log('hasHardwareAuth', res)
+
+          this.setState({hasHardwareAuth: res})
+
+          if (res) {
+            LocalAuthentication.supportedAuthenticationTypesAsync()
+                .then(res => {
+                  console.log('supportedAuthenticationTypes', res)
+
+                  const hasFingerPrint = res.includes(1);
+                  const hasFaceUnlock = res.includes(2);
+
+                  this.setState({
+                    hasFingerPrint,
+                    hasFaceUnlock
+                  })
+                })
+                .catch(error => {console.error(error)})
+          }
+
+        })
+        .catch(error => {console.error(error)})
+  }
+
+  componentDidMount() {
+    this._checkHardwareAuth()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (!prevState.hasHardwareAuth && this.state.hasHardwareAuth) {
+      console.log('Device supports hardware auth')
+
+    }
+  }
+
   render() {
     console.log('ZERTY', persistorStore);
     return (
@@ -19,6 +68,8 @@ class Root extends Component {
           <PersistGate loading={null} persistor={persistorStore.persistor}>
 
             <Text>Expo Tester</Text>
+
+
 
             <MainScreen/>
 
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import {connect} from 'react-redux'
+import { LocalAuthentication } from 'expo'
 
 class MainScreen extends Component {
   render() {
